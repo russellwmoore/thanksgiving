b460e9da7773639b993d71a8b744c4e2b5bbadc1
HypeDis <m.bae@live.com>
2019-11-14 01:33:48 -0500

    created models

--- db/models/Dish.js
+++ db/models/Dish.js
@@ -1,6 +1,17 @@
 const Sequelize = require('sequelize');
 const { db } = require('../connection');
 
-const Dish = db.define('dish', {});
+const Dish = db.define('dish', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+});
 
 module.exports = { Dish };
--- db/models/Person.js
+++ db/models/Person.js
@@ -1,6 +1,18 @@
 const Sequelize = require('sequelize');
 const { db } = require('../connection');
 
-const Person = db.define('person', {});
+const Person = db.define('person', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  isAttending: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: true,
+  },
+});
 
 module.exports = { Person };
